b08567e14b6442cec85e1cccd223cbf32fc27125
け <challengerk.8000@gmail.com>
2019-07-13 14:21:16 +0900

    Update ts2mp4.sh

--- ts2mp4.sh
+++ ts2mp4.sh
@@ -19,14 +19,29 @@ ENC_TYPE="m2ts"
 # FFmpegのコマンド。通常は変更しなくて良い。
 FFMPEG_CMD="ffmpeg"
 
+###########
 # 以下処理
-cd ${RECORDED_DIR}
-for VIDEO_PATH in ${RECORDED_DIR}*; do
-    VIDEO_FILE=$(basename ${VIDEO_PATH})
-    FILE_TYPE="${VIDEO_FILE##*.}"
-    MP4_FILE="${MP4_DIR}${VIDEO_FILE%.*}.mp4"
+echo Start encoding.
 
+# フォルダ作成
+mkdir -p -v ${RECORDED_DIR}
+mkdir -p -v ${MP4_DIR}
+cd ${RECORDED_DIR}
+
+# 対象フォルダ配下の全てのファイルパスを取得
+for VIDEO_PATH in ${RECORDED_DIR}*; do
+    # 変換元ファイル名
+    VIDEO_FILE=$(basename ${VIDEO_PATH})
+    
+    # 変換後ファイル名
+    MP4_FILE="${MP4_DIR}${VIDEO_FILE%.*}.mp4"
+    
+    # ファイル拡張子
+    FILE_TYPE="${VIDEO_FILE##*.}"
+
+    # エンコード対象拡張子かつMP4ファイルが未作成の場合エンコード
     if [ ${FILE_TYPE} = ${ENC_TYPE} ] && [ ! -e ${MP4_FILE} ]; then
         ${FFMPEG_CMD} -y -i ${RECORDED_DIR}${VIDEO_FILE} -vcodec h264_omx -b:v 6000k -vf pullup -r 24000/1001 -vsync 1 -qmin 10 -qmax 51 -qdiff 8 -i_qfactor 1.40 -qcomp 0.6 ${MP4_FILE}
     fi
 done
+echo Finished encoding.
