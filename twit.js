a20e0da167951f8b320de071ff4bc7a3631a9a1e
け <challengerk.8000@gmail.com>
2019-07-14 15:59:57 +0900

    Create twit.js

---
+++ twit.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Mtwitter = require('mtwitter');
+
+const tweeter = new Mtwitter({
+    consumer_key       : xxxx,
+    consumer_secret    : xxxx,
+    access_token_key   : xxxx,
+    access_token_secret: xxxx
+});
+
+const tweeterUpdater = (status) => {
+    tweeter.post(
+        '/statuses/update',
+        { status: status },
+        (err, _item) => {
+            if (err) {
+                console.error('[Tweeter] Error: ' + JSON.stringify(err));
+            } else {
+                console.log('[Tweeter] Updated: ' + status);
+            }
+        }
+    );
+};
+
+tweeterUpdater(process.argv[2]);
