d66cb66fd693c00363e037182696b2ad07ac4c80
け <challengerk.8000@gmail.com>
2022-07-18 20:41:32 +0900

    Update 04-setup-mirakurun.sh

--- setup/04-setup-mirakurun.sh
+++ setup/04-setup-mirakurun.sh
@@ -5,11 +5,16 @@ curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # PM2のインストール
-sudo npm install pm2 -g
+sudo npm install pm2 --location=global
 sudo pm2 startup
 
 # Mirakurunのインストール
-sudo npm install mirakurun -g --unsafe-perm --foreground-scripts --production
+sudo npm install mirakurun@3.8.1 --location=global --production
+sudo mirakurun init
+
+# v3.8.1を経由しないと3.9.xに上げられない
+sudo npm install mirakurun --location=global --production
+sudo mirakurun restart
 
 # Node.jsのバージョン確認
 node -v
