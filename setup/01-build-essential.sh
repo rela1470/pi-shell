22272f6e9109d4cde12ffc8fe4fe76634c753b6f
け <challengerk.8000@gmail.com>
2022-07-18 18:03:10 +0900

    Update 01-build-essential.sh

--- setup/01-build-essential.sh
+++ setup/01-build-essential.sh
@@ -1,8 +1,28 @@
 # パッケージの更新
 sudo apt-get update
+sudo apt-get upgrade -y
 
 # 必要なパッケージをインストール
-sudo apt-get -y install build-essential automake pkg-config git vim unzip cmake raspberrypi-kernel raspberrypi-kernel-headers
+sudo apt-get install -y --no-install-recommends \
+    make \
+    gcc \
+    g++ \
+    pcscd \
+    pcsc-tools \
+    dvb-tools \
+    libccid \
+    libdvbv5-dev \
+    libpcsclite-dev \
+    build-essential \
+    automake \
+    pkg-config \
+    wget \
+    git \
+    vim \
+    unzip \
+    cmake \
+    raspberrypi-kernel \
+    raspberrypi-kernel-headers
 
 # 一旦再起動
 sudo reboot
