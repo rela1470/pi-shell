4dd581d6e29b0d73616dc48114885bb0ac4b5f2e
け <challengerk.8000@gmail.com>
2022-07-18 16:56:55 +0900

    Create setup-build-essential.sh

---
+++ setup-build-essential.sh
@@ -0,0 +1,8 @@
+# パッケージの更新
+sudo apt-get update
+
+# 必要なパッケージをインストール
+sudo apt-get -y install build-essential automake pkg-config git vim unzip cmake raspberrypi-kernel raspberrypi-kernel-headers
+
+# 一旦再起動
+sudo reboot
